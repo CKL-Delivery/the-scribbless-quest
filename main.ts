7762f3bb9f0fe95dd233c48d0d07d644f8365dee
CKL-Delivery <83294953+CKL-Delivery@users.noreply.github.com>
2021-05-18 07:29:55 -0700

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,36 @@
 namespace SpriteKind {
     export const NPC = SpriteKind.create()
 }
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.buttonPink, function (sprite, location) {
+    mySprite.setPosition(10, 9)
+    tiles.setTilemap(tilemap`level2`)
+})
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (mySprite.overlapsWith(mySprite3)) {
+        game.splash("Hi! I heard you two talking")
+        game.splash("Here's the sword!")
+    }
+})
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (mySprite.overlapsWith(mySprite2)) {
         game.splash("Seeing you out there, solving ", "that puzzle...")
-        game.splash("it just set something off in me.", "I wanna join you!")
+        game.splash("I think you can help us!", "You see there's a monster...")
+        game.splash("AH!!! I'm scared just thinking about it!", "Can you help us?")
+        game.splash("There's this yellow guy.", "He's the only one of us with a sword")
+        game.splash("Go talk to him.", "Save us!")
+        tiles.setTilemap(tilemap`level3`)
     }
 })
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.hazardLava1, function (sprite, location) {
+    game.over(false, effects.melt)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.buttonOrange, function (sprite, location) {
+    game.over(false)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.buttonTeal, function (sprite, location) {
+    game.over(false)
+})
+let mySprite3: Sprite = null
 let mySprite2: Sprite = null
 let mySprite: Sprite = null
 mySprite = sprites.create(img`
@@ -27,7 +51,7 @@ mySprite = sprites.create(img`
     . . . 2 2 2 . . . 2 . . . . . . 
     . . . 2 2 . . . . . . . . . . . 
     `, SpriteKind.Player)
-mySprite.setPosition(73, 30)
+mySprite.setPosition(14, 16)
 controller.moveSprite(mySprite)
 tiles.setTilemap(tilemap`level1`)
 scene.cameraFollowSprite(mySprite)
@@ -49,8 +73,8 @@ mySprite2 = sprites.create(img`
     . . 8 8 . . . . 8 . . . . 8 . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.NPC)
-mySprite2.setPosition(143, 68)
-let mySprite3 = sprites.create(img`
+mySprite2.setPosition(129, 68)
+mySprite3 = sprites.create(img`
     . . . . . . . . . . . . . . 5 5 
     . . . . . 5 5 . . . . . 5 5 5 . 
     . . . 5 5 . . 5 5 5 5 5 . 5 . . 
@@ -68,4 +92,3 @@ let mySprite3 = sprites.create(img`
     . 5 5 5 5 . . 5 5 5 . 5 5 . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.NPC)
-mySprite3.setPosition(78, 57)
